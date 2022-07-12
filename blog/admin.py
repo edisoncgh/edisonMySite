64135fe5227c14a6780bd4deb17db33b92956ad9
edisoncgh <1009808474@qq.com>
2022-07-13 01:05:16 +0800

    添加元数据段信息，完善admin面板

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 # 系统后台
 from django.contrib import admin
 
+from .models import Article, Category, Comment, Friendlink
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Friendlink)
--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,7 @@ class User(models.Model):
     avatar = models.ImageField(
         upload_to='media', default="media/default.jpg")  # 用户头像
 
+    @classmethod
     def __str__(self):
         return self.username
 
@@ -33,6 +34,7 @@ class User(models.Model):
         self.comment_num -= 1
         self.save(update_fields=['comment_num'])
 
+    # 模型元数据选项
     class Meta:
         verbose_name = '用户'  # 指定后台显示模型名称
         verbose_name_plural = '用户'  # 指定后台显示模型复数名称
@@ -46,6 +48,10 @@ class UserAdmin(admin.ModelAdmin):
 
 
 class Article(models.Model):
+    STATUS_CHOICES = (
+        ('d', '草稿'),
+        ('p', '已发表'),
+    )
     article_id = models.IntegerField(
         verbose_name='文章ID', primary_key=True)  # 主键，自增
     article_cat = models.CharField(
@@ -58,6 +64,31 @@ class Article(models.Model):
     like_num = models.PositiveIntegerField(verbose_name='点赞数', default=0)
     visit_num = models.PositiveIntegerField(verbose_name='浏览数', default=0)
 
+    # 使对象在后台显示更友好
+    @classmethod
+    def __str__(self):
+        return self.title
+
+    # 更新浏览量
+    @classmethod
+    def visited(self):
+        self.visit_num += 1
+        self.save(update_fields=['visit_num'])
+
+    # 下一篇
+    @classmethod
+    def next_article(self):  # id比当前id大，状态为已发布，发布时间不为空
+        return Article.objects.filter(id__gt=self.article_id, status='p', pub_time__isnull=False).first()
+
+    # 前一篇
+    @classmethod
+    def prev_article(self):  # id比当前id小，状态为已发布，发布时间不为空
+        return Article.objects.filter(id__lt=self.article_id, status='p', pub_time__isnull=False).first()
+
+    # 模型元数据选项
+    class Meta:
+        db_table = "articles"  # 数据库表名
+        ordering = ['-publish_date']  # 按发布日志降序排序
 # ---------------------------分类表---------------------------
 
 
@@ -68,22 +99,45 @@ class Category(models.Model):
     cat_name = models.CharField(
         verbose_name='类别名称', max_length=20)
 
+    @classmethod
+    # 使对象在后台显示更友好
+    def __str__(self):
+        return self.cat_name
+
+    # 模型元数据选项
+    class Meta:
+        db_table = "categories"  # 数据库表名
+
 # ---------------------------评论表---------------------------
 
 
 class Comment(models.Model):
+    STATUS_CHOICES = (
+        ('f', '待通过'),
+        ('t', '已通过'),
+    )
     article = models.ForeignKey(Article, on_delete=models.CASCADE)  # 外键，指向所属文章
     comment_id = models.IntegerField(
         verbose_name='评论ID', primary_key=True)  # 主键，自增
-    comment_author = models.CharField(
+    author = models.CharField(
         verbose_name='评论作者', max_length=20)
-    comment_author_email = models.EmailField(
+    author_email = models.EmailField(
         verbose_name='作者邮箱')
-    comment_author_link = models.URLField(
+    author_link = models.URLField(
         verbose_name='作者链接', default='')
     comment_date = models.DateTimeField(
         verbose_name='评论日期', default=datetime.datetime.now())
+    content = models.TextField(verbose_name='评论正文', blank=True, null=True)
 
+    @classmethod
+    # 使对象在后台显示更友好
+    def __str__(self):
+        return self.content
+
+    # 模型元数据选项
+    class Meta:
+        db_table = "comments"  # 数据库表名
+        ordering = ['-comment_date']  # 按发布日志降序排序
 # ---------------------------友链表---------------------------
 
 
@@ -98,3 +152,12 @@ class Friendlink(models.Model):
         verbose_name='友链名称', max_length=20)
     link_avatar = models.ImageField(
         verbose_name='友链头像', upload_to='media', default="media/default.jpg")
+
+    @classmethod
+    # 使对象在后台显示更友好
+    def __str__(self):
+        return self.link_name
+
+    # 模型元数据选项
+    class Meta:
+        db_table = "friendlinks"  # 数据库表名
